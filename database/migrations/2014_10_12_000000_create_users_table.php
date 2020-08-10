a3817faf1f2b022b051b4ce57410d51abf25961a
Ashish Keshari <kesari.uday12@gmail.com>
2020-08-10 18:09:55 +0530

    Added missing columns in users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,17 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('home_number');
+            $table->string('address');
+            $table->string('locality');
+            $table->string('city');
+            $table->integer('zip_code');
+            $table->string('location');
+            $table->boolean('is_active')->default(0);
+            $table->integer('phone')->unique();
+            $table->integer('phone_verified_at')->nullable(); 
+            $table->string('created_by');
+            $table->boolean('is_admin')->default(0);        
             $table->rememberToken();
             $table->timestamps();
         });
